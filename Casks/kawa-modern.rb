fca7749f37e233fcfa1eee9866ed7edcc82bc43a
Andrii Honcharov <dev@honcharov.pro>
2026-09-14 10:47:50 +0300

    Update kawa-modern to v2.0.3

--- Casks/kawa-modern.rb
+++ Casks/kawa-modern.rb
@@ -1,6 +1,6 @@
 cask "kawa-modern" do
-  version "2.0.2"
-  sha256 "81ad29b943dd7d809dec8602e0ed945b1f1de784bc1b6bab3a489c3ea805c321"
+  version "2.0.3"
+  sha256 "892efbcf1f63ad3651063df8535320c0fac313d5de035a6aebceb82bb74b28e4"
 
   url "https://github.com/ghotriw/kawa-modern/releases/download/v#{version}/Kawa-Modern.zip"
   name "Kawa Modern"
